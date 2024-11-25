6981b0f541f81d0217e19374cfe1cdbf28ec480c
Yevgeniy Kadranov <yk@ice-service.net>
2024-11-25 14:11:32 +0500

    adding data and req

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from datetime import datetime
+import os
+
+from PIL import Image
+st.set_page_config(layout = 'wide')
+
+from streamlit_image_comparison import image_comparison
+
+col_img, col_leg = st.columns((1,1))
+with col_img:
+    image_comparison(f'./data/images/S1A_20241125.png',
+                    f'./data/images/S2B_20241031.png')
